d7b634a18aea62f37f89d1d45f691f17fee10564
Prafulpatekar <prafulpatekar23@gmail.com>
2022-07-29 21:26:27 +0300

    first commit

--- praful_coder_portfolio/settings.py
+++ praful_coder_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-whdh!d&9b!)+a9r@-pgksjy$syx#)jx_vi)dl+onr7en=plv&^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
