ae46881c7ca5ff40173f88bcf6a9784b06f4b3f3
Praful Patekar <prafulpatekar23@gmail.com>
2025-01-09 00:06:48 +0530

    DB PORT Correction

--- myportfolio/myportfolio/settings/base.py
+++ myportfolio/myportfolio/settings/base.py
@@ -73,7 +73,7 @@ DATABASES = {
         "USER": "myportfolio",
         "PASSWORD": "myportfolio",
         "HOST": "localhost",  # will be changed when running in docker
-        "PORT": "5433",
+        "PORT": "5432",
         "ATOMIC_REQUESTS": True,
         "CONN_MAX_AGE": 0,
     }
